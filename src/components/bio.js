b2863466ca6462d5e0bfcb8c7f370f4492c9a659
Davi Sena <davisena145@gmail.com>
2025-03-08 13:38:41 -0800

    Fixing profile picture

--- src/components/bio.js
+++ src/components/bio.js
@@ -36,7 +36,7 @@ const Bio = () => {
         className="bio-avatar"
         layout="fixed"
         formats={["auto", "webp", "avif"]}
-        src="./profile-pic.jpg"
+        src="../images/idol.JPG"
         width={50}
         height={50}
         quality={95}
