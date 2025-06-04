01b58760a9ccf5709286f26032401509b237b71e
Davi Sena <davisena145@gmail.com>
2025-06-03 22:42:30 -0700

    Making it mobile friendly

--- src/pages/index2.js
+++ src/pages/index2.js
@@ -36,6 +36,20 @@ const BlogIndex = ({ data, location, pageContext }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`;
   const posts = data.allMarkdownRemark.nodes;
 
+  // Add containerStyle for mobile friendliness
+  const containerStyle = {
+    padding: "0 16px",
+    maxWidth: "100%",
+    boxSizing: "border-box",
+    margin: "0 auto",
+    '@media (min-width: 768px)': {
+      maxWidth: "90%"
+    },
+    '@media (min-width: 1024px)': {
+      maxWidth: "1000px"
+    }
+  };
+
   if (posts.length === 0) {
     return (
       <Layout location={location} title={siteTitle}>
@@ -52,38 +66,42 @@ const BlogIndex = ({ data, location, pageContext }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <Bio />
-      <ol style={{ listStyle: `none` }}>
-        {posts.map(post => {
-          const title = post.frontmatter.title || post.fields.slug;
+      <div style={containerStyle}>
+        <ol style={{ listStyle: `none`, padding: 0, margin: 0 }}>
+          {posts.map(post => {
+            const title = post.frontmatter.title || post.fields.slug;
 
-          return (
-            <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                  <small>{post.frontmatter.date}</small>
-                </header>
-                <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: post.frontmatter.description || post.excerpt,
-                    }}
-                    itemProp="description"
-                  />
-                </section>
-              </article>
-            </li>
-          );
-        })}
-      </ol>
+            return (
+              <li key={post.fields.slug}>
+                <article
+                  className="post-list-item"
+                  style={{ marginBottom: "2rem", padding: "1rem 0" }}
+                  itemScope
+                  itemType="http://schema.org/Article"
+                >
+                  <header>
+                    <h2 style={{ fontSize: "clamp(1.2rem, 5vw, 1.5rem)", lineHeight: 1.3 }}>
+                      <Link to={post.fields.slug} itemProp="url">
+                        <span itemProp="headline">{title}</span>
+                      </Link>
+                    </h2>
+                    <small>{post.frontmatter.date}</small>
+                  </header>
+                  <section>
+                    <p
+                      style={{ fontSize: "0.95rem", lineHeight: 1.6 }}
+                      dangerouslySetInnerHTML={{
+                        __html: post.frontmatter.description || post.excerpt,
+                      }}
+                      itemProp="description"
+                    />
+                  </section>
+                </article>
+              </li>
+            );
+          })}
+        </ol>
+      </div>
     </Layout>
   );
 };
